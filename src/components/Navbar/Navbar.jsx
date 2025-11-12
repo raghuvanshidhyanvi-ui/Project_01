0c5d30eec6f7ec7ff1ac509007d23dd38e235747
Dhyanvi <dhyanvi8103@gmail.com>
2025-11-12 14:52:31 +0530

    Completed frontend and fixed StoreContext error

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,28 +1,32 @@
-import React , {useState} from 'react'
+import React , {useState , useContext} from 'react'
 import './Navbar.css'
 import { assets } from '../../assets/assets'
+import { Link } from 'react-router-dom'
+import { StoreContext } from '../../context/StoreContext'
 
-const Navbar = () => {
+const Navbar = ({setShowLogin}) => {
 
 const [menu , setMenu] = useState("home");
 
+
+const {getTotalCartAmount} = useContext(StoreContext)
   return (
     <div className='navbar'>
-    <img className='logo' src={assets.logo}></img>
+   <Link to="/"><img className='logo' src={assets.logo}></img></Link> 
     <ul className='navbar-menu'>
-      <li onClick={()=>setMenu("home")} className={menu==="home"?"active":""} >home</li>
-      <li onClick={()=>setMenu("menu")}  className={menu==="menu"?"active":""} >menu</li>
-      <li onClick={()=>setMenu("mobile-app")}  className={menu==="mobile-app"?"active":""} >mobile-app</li>
-      <li onClick={()=>setMenu("contact-us")}  className={menu==="contact-us"?"active":""} >contact us</li>
+      <Link to="/" onClick={()=>setMenu("home")} className={menu==="home"?"active":""} >home</Link>
+      <a href='#explore-menu' onClick={()=>setMenu("menu")}  className={menu==="menu"?"active":""} >menu</a>
+      <a href='#app-download' onClick={()=>setMenu("mobile-app")}  className={menu==="mobile-app"?"active":""} >mobile-app</a>
+      <a href='#footer' onClick={()=>setMenu("contact-us")}  className={menu==="contact-us"?"active":""} >contact us</a>
     </ul>
 
     <div className='navbar-right'>
       <img src={assets.search_icon}></img>
       <div className='navbar-search-icon'>
-        <img src={assets.basket_icon}></img>
-        <div className='dot'></div>
+       <Link to="/cart"><img  src={assets.basket_icon}></img></Link> 
+        <div className={getTotalCartAmount()===0?"":"dot"}></div>
       </div>
-      <button>sign in</button>
+      <button onClick={()=>setShowLogin(true)}>sign in</button>
       </div>    
 
       
